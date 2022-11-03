fdb6ab17f8feae89411f944c0913d3c0480b8876
olli1110 <86953124+olli1110@users.noreply.github.com>
2022-11-03 21:23:40 +0100

    Add files via upload

--- src/main/java/application/PlayWithStrings.java
+++ src/main/java/application/PlayWithStrings.java
@@ -1,8 +1,16 @@
 package application;
 
+import java.util.Objects;
+
 public class PlayWithStrings {
 	public static void main(String[] args) {
 		System.out.println("Hello String World");
+
+		printIntitials("Oliver", "Fucec");
+
+		System.out.println(areAnagrams("parliament".replaceAll("\\s+",""), "partial men".replaceAll("\\s+",""))); 
+
+		System.out.println(isPalindrome("Eva can I see bees in a cave".replaceAll("\\s+","")));
 	}
 
 	/**
@@ -15,6 +23,12 @@ public class PlayWithStrings {
 	 */
 	public static void printIntitials(String firstname, String lastname) {
 
+		StringBuilder builder = new StringBuilder();
+		builder.append(firstname);
+		builder.append(" ");
+		builder.append(lastname);
+		System.out.println(builder);
+		return;
 	}
 
 	/**
@@ -31,11 +45,16 @@ public class PlayWithStrings {
 	 * @return true if the values are anagrams, false otherwise.
 	 */
 	public static boolean areAnagrams(String value1, String value2) {
-		return false;
+		return sumOfCharCodes(value1) == sumOfCharCodes(value2);
 	}
 
+	
+    public static int sumOfCharCodes(String string) {
+        return (Objects.nonNull(string) ? (string.matches("(?i)[a-z]*+") ? string.toUpperCase() : "") : "").chars().sum();
+    }
+
 	/**
-	 * TODO: palindrone checker
+	 * TODO: palindrome checker
 	 * 
 	 * This method should find out if a string value is a palindrome or not. A
 	 * palindrome is a sequence of characters (can also be a number) which reads the
@@ -49,6 +68,17 @@ public class PlayWithStrings {
 	 * @return true if it is a palindrome, false otherwise.
 	 */
 	public static boolean isPalindrome(String value) {
-		return false;
+		char[] ch = new char[value.length()];
+		boolean returnValue = false;
+		for(int i = 0; i < value.length(); i++){
+			ch[i] = value.charAt(i);
+		}
+		for(int i = 0; i < ch.length; i++){
+			for(int j = ch.length - 1; j > i; j--){
+				if(ch[i] == ch[j - 1])	returnValue = true;
+				else returnValue = false;
+			}
+		}
+		return returnValue;
 	}
 }
